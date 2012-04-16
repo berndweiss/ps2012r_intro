2d6efb3f4d908a2b36c984d4c5cf6b408ab3c0b0
berndweiss <spam@metaanalyse.de>
2012-04-16 08:55:32 +0100

    modify title page, add a few slides on working with data, minor changes

--- test/test.R
+++ test/test.R
@@ -1,3 +1,9 @@
+
+## [[file:e:/projects/software/ps2012r_intro/test/test.org::*TEST%202][TEST-2:1]]
+
+setwd("E:/projects/software/ps2012r_intro/test")
 pdf("testplot.pdf")
 hist(rnorm(100))
 dev.off()
+
+## TEST-2:1 ends here
